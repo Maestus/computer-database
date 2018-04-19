34e5291fb5a995afe5d7a8b6fcc97d66b30f755d
Rached Ben Sassi <rbensassi@excilys.com>
2018-04-19 12:10:00 +0200

    Mise en place d'un controller

---
+++ src/com/excilys/cdb/service/ComputerService.java
@@ -0,0 +1,61 @@
+package com.excilys.cdb.service;
+
+import com.excilys.cdb.dao.CompanyDAO;
+import com.excilys.cdb.dao.ComputerDAO;
+import com.excilys.cdb.dao.DAOFactory;
+import com.excilys.cdb.model.Computer;
+import com.excilys.cdb.utils.Page;
+
+public class ComputerService {
+	
+	private ComputerDAO computerDao;
+	private CompanyDAO companyDao;
+
+	
+	public void init(DAOFactory dao) {
+		this.computerDao = new ComputerDAO(dao);
+		this.companyDao = new CompanyDAO(dao);
+	}
+	
+	public Page<Computer> getListComputer(int offset, int nbElem) {
+		return computerDao.findAll(offset, nbElem);
+	}
+	
+	public Page<Computer> getListComputerByCompany(int offset, int nbElem, long id) {
+		return computerDao.findByCompanyId(offset, nbElem, id);
+	}
+
+	public Computer getComputerById(Long id) {
+		return (Computer) computerDao.findById(id);
+	}
+	
+	public void addComputer(Computer c) {
+		if(c.getCompanyId() != null) {
+			if(companyDao.findById((long) c.getCompanyId()) != null) {
+				computerDao.create(c);
+			} else {
+				System.err.println("Campany inexistante");
+			}
+		} else {
+			computerDao.create(c);
+		}
+	}
+	
+	public void updateComputer(Computer c) {
+		if(c.getCompanyId() != null) {
+			if(companyDao.findById((long) c.getCompanyId()) != null) {
+				computerDao.update(c);
+			} else {
+				System.err.println("Campany inexistante");
+			}
+		} else {
+			computerDao.update(c);
+		}
+	}
+
+	public void removeComputer(Computer computer) {
+		System.out.println(computer.getId());
+		computerDao.delete(computer.getId());
+	}
+}
+ 
